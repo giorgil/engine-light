d612445971ccc2dbb6142577b284b70e422088a1
Trek Glowacki <pietrekg@umich.edu>
2008-09-26 11:00:35 -0400

    Refactored controller's 'context' attribute to 'view'.  self.view['person'] = Person.get_by_id(2) just makes more sense to me

--- app/controllers/foos_controller.py
+++ app/controllers/foos_controller.py
@@ -3,13 +3,13 @@ from app.models.greeting import Greeting
 class FoosController(ApplicationController):
   def index(self):
     """docstring for index"""
-    self.context['greetings'] = Greeting.all().fetch(10)
+    self.view['greetings'] = Greeting.all().fetch(10)
     self.render_view('foos/index.html')
   
   def show(self):
     """docstring for show"""
     Greeting.get_by_id(2)
-    self.context['greeting'] = Greeting.get_by_id(2)
-    self.context['x'] = self.request.arguments()
+    self.view['greeting'] = Greeting.get_by_id(2)
+    self.view['x'] = self.request.arguments()
     self.render_view('foos/show.html')
   
--- framework/controller/abstract_controller.py
+++ framework/controller/abstract_controller.py
@@ -6,13 +6,13 @@ class AbstractController():
     """docstring for __init"""
     self.request = request
     self.response = response
-    self.context =  {}
+    self.view =  {}
   
   def render_view(self, file_name):
     """docstring for render"""
-    path = os.path.join(APP_ROOT, ('app/views/' + file_name) )
+    path = (os.path.join(APP_ROOT, ('app/views/' + file_name) ))
     self.response.headers['Content-Type'] = 'text/html'
-    self.response.out.write(template.render(path, self.context))
+    self.response.out.write(template.render(path, self.view))
     
   def render_text(self, text):
     """docstring for render_text"""
@@ -23,6 +23,12 @@ class AbstractController():
     """docstring for render_xml"""
     self.response.headers['Content-Type'] = 'text/xml'
 
+  def _default_view(self):
+    """Used interally to render a default view when render is called without any arguments so
+       a request that maps to {controller="foos", action="show", id='10'} will render the file
+       /app/views/foos/show.html
+    """
+    pass
     
     
     
